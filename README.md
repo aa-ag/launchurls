b4f0d9ca99161730705e5ad236c7675de1e7a4fe
aa-ag <aaron.aguerrevere@gmail.com>
2020-12-20 20:47:00 -0600

    Creating CSV file

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 Python script to launch multiple URL's all at once from an excel file. 
 
 `script.py` will use Github's API to grab all URL's (repos) from a user's account and save them into a CSV or other type of file. Then, it will launch them through the browser all at the same time. 
+
+#### TO DO
+
+- Write columns instead of rows.
+- Write function to launch all of them at the same time.
--- script.py
+++ script.py
@@ -1,6 +1,7 @@
 import requests
 from pprint import pprint
 import settings
+import csv
 
 
 token = settings.GITHUB_TOKEN
@@ -10,5 +11,16 @@ r = requests.get("https://api.github.com/users/aa-ag/repos")
 if r.status_code == 200:
     all_repos = r.json()
 
+    names = list()
+    urls = list()
+
     for repo in all_repos:
-        print(repo['name'] + ' | ' + repo['svn_url'])
+        names.append(repo['name'])
+        urls.append(repo['svn_url'])
+
+    filename = "all_my_repos.csv"
+
+    with open(filename, 'w') as csv_file:
+        csvwriter = csv.writer(csv_file)
+        csvwriter.writerow(names)
+        csvwriter.writerow(urls)
